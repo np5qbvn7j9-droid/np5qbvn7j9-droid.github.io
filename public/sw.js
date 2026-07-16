ce1c80bda351f8f82b56d023c0ba462a40ded904
homvisit <np5qbvn7j9@privaterelay.appleid.com>
2026-07-16 06:05:25 +0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+// Simple offline-first service worker: cache app shell, network-first for navigation
+const CACHE = 'maktabati-v1'
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url)
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone()
+        caches.open(CACHE).then((c) => c.put(e.request, copy))
+        return res
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  )
+})
